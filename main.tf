31d5cdaafcda08f2034767dcbc3f00e5883122a2
TsonasIoannis <32147385+TsonasIoannis@users.noreply.github.com>
2023-11-22 19:04:58 +0000

    chore: add vscode settings

--- main.tf
+++ main.tf
@@ -4,9 +4,30 @@ terraform {
       source  = "hashicorp/google"
       version = "5.7.0"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "4.17.0"
+    }
+    kubernetes = {
+      version = "2.23.0"
+    }
+    helm = {
+      version = "2.11.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    grafana = {
+      source  = "grafana/grafana"
+      version = "2.3.3"
+    }
   }
 }
 
 
 provider "google" {
+}
+
+provider "cloudflare" {
 }
