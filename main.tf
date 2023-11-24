5d5a5d2b28faf8a7c057cdb82c479eb36ec8d854
TsonasIoannis <32147385+TsonasIoannis@users.noreply.github.com>
2023-11-24 17:50:32 +0000

    chore: tflint fix

--- main.tf
+++ main.tf
@@ -9,9 +9,11 @@ terraform {
       version = "4.17.0"
     }
     kubernetes = {
+      source  = "hashicorp/kubernetes"
       version = "2.23.0"
     }
     helm = {
+      source  = "hashicorp/helm"
       version = "2.11.0"
     }
     random = {
